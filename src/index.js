6de5731f02f0761aee5e792016903061656fed50
Andrey Kleshenko <andreykleshenko@gmail.com>
2022-02-24 23:06:43 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let strLength = str.length;
+  for (let i = 0; i < strLength / 2; i++) {
+    bracketsConfig
+      .map((el) => el.join(''))
+      .map((el) => {
+        str = str.replace(el, '');
+      });
+  }
+  return str.length === 0 ? true : false;
+};
